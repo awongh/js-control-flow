60776f23816e86b8302ddec753ac1e85e0174b35
Kok Chee Kean <kok.chee.kean@outlook.com>
2018-02-27 16:28:59 +0800

    Updated filterLongWords to use the native filter function

--- js/filterLongWords.js
+++ js/filterLongWords.js
@@ -3,10 +3,8 @@ const maxLength = 9;
 
 function filterLongWords(arr, maxLength) {
     if (typeof(maxLength) != "number" || parseInt(maxLength) != maxLength || maxLength < 0) {console.error("Need a positive number for max length!"); return};
-    let result = [];
-    arr.map((currentWord) => {if (currentWord.length < maxLength) {result.push(currentWord)}});
     console.log(maxLength);
-    return result;
+    return arr.filter(word => word.length > maxLength)
 }
 
 filterLongWords(exampleData, maxLength);
