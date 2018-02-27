0a2bafd61bbd3c402a2cc6625c962fc0d1ff6319
CK <kok.chee.kean@outlook.com>
2018-02-28 00:22:43 +0800

    Alternate implementation of reverse.js

--- js/reverse.js
+++ js/reverse.js
@@ -10,4 +10,14 @@ function reverseString(num) {
     return result;
 }
 
+function reverseStringAlt(num) {
+    // Can't decide if this implementation would be faster. What happens if we want to reverse an entire novel? :D
+    let result = "";
+    for (let i = num.length - 1; i >= 0; i--) {
+        result += num.charAt(i);
+    }
+    return result
+}
+
 console.log(reverseString(inputString));
+console.log(reverseStringAlt(inputString));
