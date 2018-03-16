94669a2807882e2033b5cb4aafdba27adca7523c
enkai87 <tanenkai@firebelly.com.sg>
2018-03-16 15:32:02 +0800

    my2ndcommit

--- fizzbuzz.js/fizzbuzz.js
+++ fizzbuzz.js/fizzbuzz.js
@@ -1,9 +1,21 @@
-var add = function(a,b){
-  return a * b;
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+var eating = function(b) {
+	var newArray = [];
+	for(var i = 1; i <= b.length; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			newArray.push("fizzbuzz");
+		}
+		else if (i % 3 === 0) {
+			newArray.push("fizz");
+		}
+		else if (i % 5 === 0) {
+			newArray.push("buzz");
+		}
+		else {
+			newArray.push(i);
+		}
+	}
+	return newArray;
 };
-
-var announce = function() {
-  console.log('Ding!');
-}
-
-var threeSecondTimeout = setTimeout(announce, 3000);
+console.log(eating(arr));
